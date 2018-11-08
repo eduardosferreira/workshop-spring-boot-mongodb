113a80ab0bcb478418ea472dd411d7b2b5147e3a
Eduardo da Silva Ferreira <eduardo.s.ferreira@gmail.com>
2018-11-08 15:14:41 -0200

    Project Created

---
+++ src/main/java/com/eduardosferreira/workshopsbmongo/WorkshopsbmongoApplication.java
@@ -0,0 +1,12 @@
+package com.eduardosferreira.workshopsbmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopsbmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopsbmongoApplication.class, args);
+	}
+}
